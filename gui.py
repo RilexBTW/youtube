34b27e5abdd9f8ba453fed7057ebc9767a57cf3f
scapezz <109783186+scapezz@users.noreply.github.com>
2022-07-22 02:58:25 -0700

    added URL widget plus color to Download button and Label widget

--- gui.py
+++ gui.py
@@ -9,27 +9,55 @@ class SayHello(App):
     def build(self):
         self.window = GridLayout()
         self.window.cols = 1
+        self.window.size_hint = (0.6, 0.7)
+        self.window.pos_hint = {"center_x": 0.5, "center_y": 0.5}
+
+        #add widget to window
+
+        #image widget
         self.window.add_widget(Image(source="logo.png"))
+
         #Label widget
-        self.greeting = Label(text="What's your name?")
+        self.greeting = Label(
+                        text ="What Would You Like to Download Today??",
+                        font_size = 38,
+                        color ='#0096FF'
+                        )
+
         self.window.add_widget(self.greeting)
+
+        #Url widget
+        self.subgreeting = Label(
+                           text ="Paste URL here",
+                           font_size = 24,
+                           color = '000080'
+                           )
+        self.window.add_widget(self.subgreeting)
+
+
         # text input widget
-        self.user = TextInput(multiline=False)
+        self.user = TextInput(
+                    multiline=False,
+                    padding_y= (20,20),
+                    size_hint = (1, 0.5)
+                    )
+
         self.window.add_widget(self.user)
+
         # Button widget
-        self.button = Button(text="GREET")
+        self.button = Button(
+                      text="Download!",
+                      size_hint = (1,0.5),
+                      bold = True,
+                      background_color ='#0096FF'
+                      )
         self.button.bind(on_press=self.callback)
         self.window.add_widget(self.button)
 
         return self.window
 
     def callback(self,instance):
-        self.greeting.text = "Hello " + self.user.text + "!"
-
-
-
-
-
+        self.greeting.text = "Downloading " + self.user.text + "!"
 
         return self.window
 
