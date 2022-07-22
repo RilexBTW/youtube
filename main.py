34b2dd6e682add0c394189aa5b270a253c899ea5
rilexbtw <101215214+RilexBTW@users.noreply.github.com>
2022-07-22 03:58:58 -0400

    commit

---
+++ main.py
@@ -0,0 +1,26 @@
+from __future__ import unicode_literals
+import youtube_dl
+
+
+
+url = 'https://www.youtube.com/watch?v=pwBFOuCrdr4'
+fileType = ''
+fileFormat = ''
+fileOutputName = ''
+fileQuality = ''
+
+
+
+
+
+def main():
+    print("placeholder placeholder placeholder")
+    download()
+
+
+
+def download():
+    print('Starting download...')
+    ydl_opts = {}
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download(url)
